216925390ec8a4967332772d87439f92025572cf
sainaya <sainaya99@gmail.com>
2023-06-09 20:00:58 -0700

    add GitHub action to launch Mayhem

--- fuzzme.c
+++ fuzzme.c
@@ -1,44 +1,27 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int fuzzme(char *buf)
-{
-  if(strlen(buf) >= 3)
-    if(buf[0] == 'b')
-      if(buf[1] == 'u')
-        if(buf[2] == 'g') {
-          printf("You've got it!");
-          abort();
-        }
+// Change the signature of fuzzme to match LLVMFuzzerTestOneInput
+int LLVMFuzzerTestOneInput(const char *data, size_t size) {
+    char buf[size + 1];
+    memcpy(buf, data, size);
+    buf[size] = '\0';
+
+    // Call the fuzzme function with the provided data
+    fuzzme(buf);
+
     return 0;
 }
 
-#define BUFSZ 256
-
-int main(int argc, char** argv)
-{
-    char buf[BUFSZ] = { 0 };
-    FILE* f = NULL;
-    size_t nr = 0;
-
-    if (2 > argc) {
-        fprintf(stderr, "usage: %s PAYLOAD\n", argv[0]);
-        return 1;
-    }
-
-    f = fopen(argv[1], "rb");
-    assert(f);
-
-    nr = fread(buf, sizeof(buf[0]), BUFSZ, f);
-    assert(nr > 0);
-    buf[BUFSZ-1] = '\0';
-
-    fuzzme(buf);
-
-    fclose(f);
-
+int fuzzme(char *buf) {
+    if (strlen(buf) >= 3)
+        if (buf[0] == 'b')
+            if (buf[1] == 'u')
+                if (buf[2] == 'g') {
+                    printf("You've got it!");
+                    abort();
+                }
     return 0;
 }
 
